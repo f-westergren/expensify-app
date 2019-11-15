4ec40e940e163ebdf9f9b164088017f3b098abde
f-westergren <folke.westergren@gmail.com>
2019-11-15 11:21:11 -0500

    Setup production webpack build

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -86,4 +86,4 @@ test('should set calendar focus on change', () => {
     const wrapper = shallow(<ExpenseForm />);
     wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused });
     expect(wrapper.state('calendarFocused')).toBe(focused);
-});
+}); 
